393e020d9841c1a69cd1473e472594dbcea519d6
Rishi Mukherjee <rishi.mukherjee@sezzle.com>
2017-11-17 04:30:02 +0530

    CSS fixed

--- gulpfile.js
+++ gulpfile.js
@@ -24,9 +24,9 @@ var gulp = require('gulp'),
     rp = require("request-promise"),
     config = { useIAM: true };
 
-var buttonUploadName = 'sezzle-widget0.3.25.js';
+var buttonUploadName = 'sezzle-widget0.3.26.js';
 var bannerUploadName = 'sezzle-banner2.1.1.js';
-var globalCssUploadName = 'sezzle-styles-global1.1.165.css';
+var globalCssUploadName = 'sezzle-styles-global1.1.169.css';
 
 gulp.task("cssupload", function() {
     // bucket base url https://d3svog4tlx445w.cloudfront.net/
