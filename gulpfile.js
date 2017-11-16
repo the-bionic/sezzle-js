c18d486e4aca766947a25ec9534c9cf1dc9466d7
Isaac Bremseth <isaac.bremseth@sezzle.com>
2017-11-15 21:01:53 -0600

    fixed event logging

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var gulp = require('gulp'),
     rp = require("request-promise"),
     config = { useIAM: true };
 
-var buttonUploadName = 'sezzle-widget0.3.21.js';
+var buttonUploadName = 'sezzle-widget0.3.47.js';
 var bannerUploadName = 'sezzle-banner2.1.1.js';
 var globalCssUploadName = 'sezzle-styles-global1.1.159.css';
 
