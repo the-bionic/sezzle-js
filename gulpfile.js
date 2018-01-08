a3f670da8c3517aff5629f41dee1240fe9758efc
Isaac Bremseth <isaac.bremseth@sezzle.com>
2018-01-08 14:48:55 -0600

    fixed issue when ignored element is a tag

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var gulp = require('gulp'),
     rp = require("request-promise"),
     config = { useIAM: true };
 
-var buttonUploadName = 'sezzle-widget0.4.25.js';
+var buttonUploadName = 'sezzle-widget0.4.26.js';
 var bannerUploadName = 'sezzle-banner2.1.1.js';
 var globalCssUploadName = 'sezzle-styles-global1.1.287.css';
 gulp.task("cssupload", function() {
