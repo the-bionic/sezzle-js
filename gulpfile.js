73f1843116a6488e4eae06fc7938b6967463538a
Isaac Bremseth <isaac.bremseth@sezzle.com>
2018-01-09 10:40:33 -0600

    added an ignored price string

--- gulpfile.js
+++ gulpfile.js
@@ -24,9 +24,9 @@ var gulp = require('gulp'),
     rp = require("request-promise"),
     config = { useIAM: true };
 
-var buttonUploadName = 'sezzle-widget0.4.26.js';
+var buttonUploadName = 'sezzle-widget0.4.27.js';
 var bannerUploadName = 'sezzle-banner2.1.1.js';
-var globalCssUploadName = 'sezzle-styles-global1.1.290.css';
+var globalCssUploadName = 'sezzle-styles-global1.1.291.css';
 gulp.task("cssupload", function() {
     // bucket base url https://d3svog4tlx445w.cloudfront.net/
     var indexPath = './dist/global-css/global.min.css'
