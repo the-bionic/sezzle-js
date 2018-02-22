065de9819074a44c1aed2a8dd4efa8bb853a9a60
Yangyun Li <yangyun.li@sezzle.com>
2018-02-21 19:36:27 -0600

    add css for little-hooligans-co.myshopify.com

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ var gulp = require('gulp'),
 
 var buttonUploadName = 'sezzle-widget0.4.57.js';
 var bannerUploadName = 'sezzle-banner2.1.1.js';
-var globalCssUploadName = 'sezzle-styles-global1.1.440.css';
+var globalCssUploadName = 'sezzle-styles-global1.1.441.css';
 gulp.task("cssupload", function() {
     // bucket base url https://d3svog4tlx445w.cloudfront.net/
     var indexPath = './dist/global-css/global.min.css'
