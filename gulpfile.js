3de1434f93f2eccd36eca97154537a782582581f
Rishi Mukherjee <rishi.mukherjee@sezzle.com>
2018-02-28 13:44:48 -0600

    Deployed new version

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var gulp = require('gulp'),
     rp = require("request-promise"),
     config = { useIAM: true };
 
-var buttonUploadName = 'sezzle-widget0.4.57.js';
+var buttonUploadName = 'sezzle-widget0.4.58.js';
 var bannerUploadName = 'sezzle-banner2.1.1.js';
 var globalCssUploadName = 'sezzle-styles-global1.1.546.css';
 gulp.task("cssupload", function() {
