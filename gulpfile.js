e35a63775c9e7c794447be5b7451fa753d293f7f
Jan-Wei Lim <jan-wei.lim@sezzle.com>
2018-05-08 10:25:43 -0500

    Added custom css for curvyswimwearusa.myshopify.com

--- gulpfile.js
+++ gulpfile.js
@@ -24,10 +24,9 @@ var gulp = require('gulp'),
     rp = require("request-promise"),
     config = { useIAM: true };
 
-var buttonUploadName = 'sezzle-widget0.4.84.js';
+var buttonUploadName = 'sezzle-widget0.4.85.js';
 var bannerUploadName = 'sezzle-banner2.1.1.js';
-var globalCssUploadName = 'sezzle-styles-global1.1.854.css';
-
+var globalCssUploadName = 'sezzle-styles-global1.1.856.css';
 
 gulp.task("cssupload", function() {
     // bucket base url https://d3svog4tlx445w.cloudfront.net/
