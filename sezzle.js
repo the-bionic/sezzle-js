381d8ee62aced5f634c80222d4999d8aa1939263
Jan-Wei Lim <jan-wei.lim@sezzle.com>
2018-06-27 17:13:04 -0500

    Added polyfill for Array.from. Our script now uses this version of Array.from every time. (Not using Array.from directly because there are some sites which polyfill Array.from differently causing our code to not work)

--- sezzle.js
+++ sezzle.js
@@ -1,79 +1,84 @@
-if(!Array.from){
-  Array.from = (function () {
-    var toStr = Object.prototype.toString;
-    var isCallable = function (fn) {
-      return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
-    };
-    var toInteger = function (value) {
-      var number = Number(value);
-      if (isNaN(number)) { return 0; }
-      if (number === 0 || !isFinite(number)) { return number; }
-      return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
-    };
-    var maxSafeInteger = Math.pow(2, 53) - 1;
-    var toLength = function (value) {
-      var len = toInteger(value);
-      return Math.min(Math.max(len, 0), maxSafeInteger);
-    };
+/*
+ * Polyfill for Array.from. This polyfill was made to support Array.from
+ * for <= ES5 versions. The code snippet was taken from this link: 
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+ * Due to a possibility of websites writing their Array.from polyfills differently, this variable
+ * is created to ensure that this script uses this particular version of Array.from
+ */
+var SezzleCustomArrayFrom = (function () {
+  var toStr = Object.prototype.toString;
+  var isCallable = function (fn) {
+    return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+  };
+  var toInteger = function (value) {
+    var number = Number(value);
+    if (isNaN(number)) { return 0; }
+    if (number === 0 || !isFinite(number)) { return number; }
+    return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+  };
+  var maxSafeInteger = Math.pow(2, 53) - 1;
+  var toLength = function (value) {
+    var len = toInteger(value);
+    return Math.min(Math.max(len, 0), maxSafeInteger);
+  };
 
-    // The length property of the from method is 1.
-    return function from(arrayLike/*, mapFn, thisArg */) {
-      // 1. Let C be the this value.
-      var C = this;
+  // The length property of the from method is 1.
+  return function from(arrayLike/*, mapFn, thisArg */) {
+    // 1. Let C be the this value.
+    var C = this;
 
-      // 2. Let items be ToObject(arrayLike).
-      var items = Object(arrayLike);
+    // 2. Let items be ToObject(arrayLike).
+    var items = Object(arrayLike);
 
-      // 3. ReturnIfAbrupt(items).
-      if (arrayLike == null) {
-        throw new TypeError("Array.from requires an array-like object - not null or undefined");
+    // 3. ReturnIfAbrupt(items).
+    if (arrayLike == null) {
+      throw new TypeError("Array.from requires an array-like object - not null or undefined");
+    }
+
+    // 4. If mapfn is undefined, then let mapping be false.
+    var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+    var T;
+    if (typeof mapFn !== 'undefined') {
+      // 5. else
+      // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
+      if (!isCallable(mapFn)) {
+        throw new TypeError('Array.from: when provided, the second argument must be a function');
       }
 
-      // 4. If mapfn is undefined, then let mapping be false.
-      var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
-      var T;
-      if (typeof mapFn !== 'undefined') {
-        // 5. else
-        // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
-        if (!isCallable(mapFn)) {
-          throw new TypeError('Array.from: when provided, the second argument must be a function');
-        }
-
-        // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
-        if (arguments.length > 2) {
-          T = arguments[2];
-        }
+      // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      if (arguments.length > 2) {
+        T = arguments[2];
       }
+    }
 
-      // 10. Let lenValue be Get(items, "length").
-      // 11. Let len be ToLength(lenValue).
-      var len = toLength(items.length);
+    // 10. Let lenValue be Get(items, "length").
+    // 11. Let len be ToLength(lenValue).
+    var len = toLength(items.length);
 
-      // 13. If IsConstructor(C) is true, then
-      // 13. a. Let A be the result of calling the [[Construct]] internal method of C with an argument list containing the single item len.
-      // 14. a. Else, Let A be ArrayCreate(len).
-      var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+    // 13. If IsConstructor(C) is true, then
+    // 13. a. Let A be the result of calling the [[Construct]] internal method of C with an argument list containing the single item len.
+    // 14. a. Else, Let A be ArrayCreate(len).
+    var A = isCallable(C) ? Object(new C(len)) : new Array(len);
 
-      // 16. Let k be 0.
-      var k = 0;
-      // 17. Repeat, while k < len… (also steps a - h)
-      var kValue;
-      while (k < len) {
-        kValue = items[k];
-        if (mapFn) {
-          A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
-        } else {
-          A[k] = kValue;
-        }
-        k += 1;
+    // 16. Let k be 0.
+    var k = 0;
+    // 17. Repeat, while k < len… (also steps a - h)
+    var kValue;
+    while (k < len) {
+      kValue = items[k];
+      if (mapFn) {
+        A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+      } else {
+        A[k] = kValue;
       }
-      // 18. Let putStatus be Put(A, "length", len, true).
-      A.length = len;
-      // 20. Return A.
-      return A;
-    };
-  }());
-}
+      k += 1;
+    }
+    // 18. Let putStatus be Put(A, "length", len, true).
+    A.length = len;
+    // 20. Return A.
+    return A;
+  };
+}());
 
 /**
  *
@@ -182,8 +187,8 @@ var SezzleJS = function(options) {
 
   // Search for price elements. If found, assume there is only one in this page
   this.hasPriceClassElement = false;
-  this.priceElements = Array.from(document.getElementsByClassName(this.priceElementClass));
-  this.renderElements = Array.from(document.getElementsByClassName(this.sezzleWidgetContainerClass));
+  this.priceElements = SezzleCustomArrayFrom(document.getElementsByClassName(this.priceElementClass));
+  this.renderElements = SezzleCustomArrayFrom(document.getElementsByClassName(this.sezzleWidgetContainerClass));
   if (this.priceElements.length == 1) this.hasPriceClassElement = true;
 
   this.theme = options.theme || '';
@@ -237,14 +242,10 @@ var SezzleJS = function(options) {
    }
 
    var children = [];
-   var ELEMENTS = Array.from(elements);
+   var ELEMENTS = SezzleCustomArrayFrom(elements);
 
-   var ELEMENTSARRAY =[];
-
-
-   var elemnt_ID = 0;
-   for(elemnt_ID in ELEMENTS) {
-     var elemnt = ELEMENTS[elemnt_ID]
+   for(var index = 0; index < ELEMENTS.length; index++) {
+     var elemnt = ELEMENTS[index]
 
      // If this is an ID
      if (xpath[xindex][0] === '#') {
@@ -252,7 +253,7 @@ var SezzleJS = function(options) {
      } else
      // If this is a class
      if (xpath[xindex][0] === '.') {
-       Array.from(
+       SezzleCustomArrayFrom(
          elemnt.getElementsByClassName(xpath[xindex].substr(1))
        )
        .forEach(function(el) {
@@ -267,7 +268,7 @@ var SezzleJS = function(options) {
            indexToTake = parseInt(xpath[xindex].split('-')[1]);
          }
        }
-       Array.from(
+       SezzleCustomArrayFrom(
          elemnt.getElementsByTagName(xpath[xindex].split('-')[0])
        )
        .forEach(function(el, index) {
@@ -849,7 +850,7 @@ SezzleJS.prototype.getPriceText = function(element) {
         subpaths = subpaths.filter(function(subpath) { return subpath !== "" });
         var queryString = "";
         //build the query string
-        for(var index in subpaths) {
+        for(var index = 0; index < subpaths.length; index++) {
           //if subpath is a tag name
           if(subpaths[index][0] !== '#' && subpaths[index][0] !== '.') {
             var splitTagAndIndex = subpaths[index].split('-');
@@ -860,7 +861,7 @@ SezzleJS.prototype.getPriceText = function(element) {
           }
         }
         if (queryString) {
-          Array.from(
+          SezzleCustomArrayFrom(
             document.querySelectorAll(queryString)
           ).forEach(function(element) {
               //mark the element to be ignored
@@ -877,16 +878,16 @@ SezzleJS.prototype.getPriceText = function(element) {
       var clone = element.cloneNode(true);
 
       //remove all marked elements
-      Array.from(
+      SezzleCustomArrayFrom(
         clone.getElementsByTagName("*")
       ).forEach(function(element) {
-        if(Array.from(element.classList).includes("sezzle-ignored-price-element")) {
+        if(SezzleCustomArrayFrom(element.classList).includes("sezzle-ignored-price-element")) {
           clone.removeChild(element);
         }
       })
 
       //remove all markers
-      Array.from(
+      SezzleCustomArrayFrom(
         document.getElementsByClassName("sezzle-ignored-price-element")
       ).forEach(function(element) {
         element.classList.remove("sezzle-ignored-price-element")
@@ -970,15 +971,15 @@ SezzleJS.prototype.deleteObserver = new MutationObserver(function(mutations) {
   if (removedAddedMutations.length) {
     // Assuming this is the mutation we need
     var removedAddedMutation = removedAddedMutations[0];
-    var removedNodes = Array.from(removedAddedMutation.removedNodes);
-    var removedNodesMutated = Array.from([]);
+    var removedNodes = SezzleCustomArrayFrom(removedAddedMutation.removedNodes);
+    var removedNodesMutated = SezzleCustomArrayFrom([]);
     removedNodesMutated = removedNodesMutated.concat(removedNodes);
 
     // Get all the removed children of deleted nodes
     for (var i=0; i<removedNodes.length; i++) {
       var removedNode = removedNodes[i];
       if ('getElementsByTagName' in removedNode) {
-        var removedChildren = Array.from(removedNode.getElementsByTagName('*'));
+        var removedChildren = SezzleCustomArrayFrom(removedNode.getElementsByTagName('*'));
         removedNodesMutated = removedNodesMutated.concat(removedChildren);
       }
     }
@@ -995,15 +996,15 @@ SezzleJS.prototype.deleteObserver = new MutationObserver(function(mutations) {
     // If the node is found, find the node corresponding node which got added
     if (removedSezzleNode) {
       var s = new SezzleJS(document.sezzleConfig);
-      var addedNodes = Array.from(removedAddedMutation.addedNodes);
+      var addedNodes = SezzleCustomArrayFrom(removedAddedMutation.addedNodes);
 
       // Store all the children of the added nodes
-      var addedNodesMutated = Array.from([]);
+      var addedNodesMutated = SezzleCustomArrayFrom([]);
       addedNodesMutated = addedNodesMutated.concat(addedNodes);
       for (var i=0; i<addedNodes.length; i++) {
         var addedNode = addedNodes[i];
         if ('getElementsByTagName' in addedNode) {
-          var addedChildren = Array.from(addedNode.getElementsByTagName('*'));
+          var addedChildren = SezzleCustomArrayFrom(addedNode.getElementsByTagName('*'));
           addedNodesMutated = addedNodesMutated.concat(addedChildren);
         }
       }
@@ -1072,7 +1073,7 @@ SezzleJS.prototype.renderModal = function() {
   }
 
   // Event listenr for click in know more button
-  Array.from(document.getElementsByClassName('sezzle-know-more'))
+  SezzleCustomArrayFrom(document.getElementsByClassName('sezzle-know-more'))
     .forEach(function(el) {
       el.addEventListener('click', function() {
         // Show modal node
@@ -1084,7 +1085,7 @@ SezzleJS.prototype.renderModal = function() {
       }.bind(this))
     }.bind(this));
 
-  Array.from(document.getElementsByClassName('sezzle-custom-widget-wrapper'))
+  SezzleCustomArrayFrom(document.getElementsByClassName('sezzle-custom-widget-wrapper'))
     .forEach(function(el) {
       if(el.getElementsByClassName('sezzle-know-more').length == 0) {
         el.parentElement.parentElement.addEventListener('click', function() {
