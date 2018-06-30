5525982bec31e65287b946e2a3e0a6bb458ecbdc
Jan-Wei Lim <jan-wei.lim@sezzle.com>
2018-06-30 17:13:27 -0500

    Bumped and deployed version 1.0.3 (Array.prototype.includes => Array.prototype.indexOf)

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var gulp = require('gulp'),
     rp = require("request-promise"),
     config = { useIAM: true };
 
-var buttonUploadName = 'sezzle-widget1.0.2.js';
+var buttonUploadName = 'sezzle-widget1.0.3.js';
 var bannerUploadName = 'sezzle-banner2.1.1.js';
 var globalCssUploadName = 'sezzle-styles-global1.1.859.css';
 
