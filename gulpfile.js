190ca49312d9753021a2336887ad785b113b4418
Jan-Wei Lim <jan-wei.lim@sezzle.com>
2018-08-22 21:23:41 -0500

    Pushed both js and css versions

--- gulpfile.js
+++ gulpfile.js
@@ -24,9 +24,9 @@ var gulp = require('gulp'),
     rp = require("request-promise"),
     config = { useIAM: true };
 
-var buttonUploadName = 'sezzle-widget1.0.3.js';
+var buttonUploadName = 'sezzle-widget1.0.4.js';
 var bannerUploadName = 'sezzle-banner2.1.1.js';
-var globalCssUploadName = 'sezzle-styles-global1.1.868.css';
+var globalCssUploadName = 'sezzle-styles-global1.1.869.css';
 
 gulp.task("cssupload", function() {
     // bucket base url https://d3svog4tlx445w.cloudfront.net/
