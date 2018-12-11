044f61bd3f4a3e329ddb693ff8a92a41d4d38609
Rishi Mukherjee <rishi.mukherjee@sezzle.com>
2018-12-10 18:27:13 -0600

    Updated the initial script to ignore fingerprint now. Adds merchant uuid.

--- initial-script.js
+++ initial-script.js
@@ -1,31 +1,15 @@
-  (function(callback) {
-    // set fingerprinting options
-    let fingerprintOptions = {
-      excludeScreenResolution: true,
-      excludeColorDepth: false,
-      excludeAvailableScreenResolution: true,
-      excludePixelRatio: true,
-      excludeWebGL: false,
-      excludeCanvas: false,
-    };
+function getUUID() {
+  var uuid = document.sezzleMerchantUUID || '';
+  if (uuid === '') {
+      // fallback if uuid does not exist
+      uuid = document.sezzleConfig["merchantID"];
+  }
+  return uuid;
+}
 
-    if(typeof window.Fingerprint2 === 'undefined' || window.Fingerprint2.VERSION != "1.4.1") {
-      var script = document.createElement("script")
-      script.type = "text/javascript";
-      script.src = 'https://cdn.jsdelivr.net/fingerprintjs2/1.4.1/fingerprint2.min.js';
-      script.onload = function() {
-        new Fingerprint2(fingerprintOptions).get(function(result, components){
-          callback(result);
-        });
-      };
-      document.getElementsByTagName("head")[0].appendChild(script);
-    } else {
-      new Fingerprint2(fingerprintOptions).get(function(result, components){
-        callback(result);
-      });
-    }
-  })(function(fingerprint) {
-    var el = document.createElement('script'); 
-    el.src = 'https://widget.sezzle.com/v1/javascript/price-widget?uuid='+document.sezzleConfig["merchantID"]+'&fingerprint='+fingerprint
-    document.getElementsByTagName('head')[0].appendChild(el);
-  });
+(function() {
+  var uuid = getUUID();
+  var el = document.createElement('script');
+  el.src = 'https://widget.sezzle.com/v1/javascript/price-widget?uuid=' + uuid
+  document.getElementsByTagName('head')[0].appendChild(el);
+})();
