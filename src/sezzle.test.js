dcedea243ff8bb1a0caa3010e6e46e8ab831f886
jan-wei.lim <jan-wei.lim@sezzle.com>
2019-06-19 14:44:52 -0500

    Fix attempt for failing tests

--- src/sezzle.test.js
+++ src/sezzle.test.js
@@ -1,20 +1,19 @@
 const SezzleJS = require('./sezzle');
 const Helper = require('./helper');
+var cloneDeep = require('lodash.clonedeep');
 var sezzleConfig = require('./sezzle.config.json');
 
 describe('Backwards compatability function works as expected', () => {
   test('Properly converts old config structure to new config structure', () => {
     // deep object comparison
     expect(sezzleConfig.new).toEqual(Helper.makeCompatible(sezzleConfig.old));
-  })
-})
+  });
+});
 
 describe('Config validator function works as expected', () => {
   test('Throws an error when configGroups is not an array', () => {
-    const newConfig = {
-      ...sezzleConfig.new
-    };
-    newConfig.configGroups = "somethingWhichIsNotAnArray";
+    const newConfig = cloneDeep(sezzleConfig.new);
+    newConfig.configGroups = 'somethingWhichIsNotAnArray';
 
     expect(() => {
       Helper.validateConfig(newConfig);
@@ -22,9 +21,7 @@ describe('Config validator function works as expected', () => {
   });
 
   test('Throws an error when configGroups is an empty array', () => {
-    const newConfig = {
-      ...sezzleConfig.new
-    };
+    const newConfig = cloneDeep(sezzleConfig.new);
     newConfig.configGroups = [];
 
     expect(() => {
@@ -33,9 +30,7 @@ describe('Config validator function works as expected', () => {
   });
 
   test('Throws an error when targetXPath is not specified in any one of the config groups', () => {
-    const newConfig = {
-      ...sezzleConfig.new
-    };
+    const newConfig = cloneDeep(sezzleConfig.new);
     delete newConfig.configGroups[0].targetXPath;
 
     expect(() => {
@@ -43,21 +38,17 @@ describe('Config validator function works as expected', () => {
     }).toThrow(new Error('targetXPath must be specified in all configs in options.configGroups'));
   });
 
-  test('Throws an error when renderToPath is not a string in any one of the config groups', () => {
-    const newConfig = {
-      ...sezzleConfig.new
-    };
+  test('Throws an error when targetXPath is not a string in any one of the config groups', () => {
+    const newConfig = cloneDeep(sezzleConfig.new);
     newConfig.configGroups[0].targetXPath = [newConfig.configGroups[0].targetXPath];
 
     expect(() => {
       Helper.validateConfig(newConfig);
-    }).toThrow(new Error('renderToPath must be of type string'));
+    }).toThrow(new Error('targetXPath must be of type string'));
   });
 
   test('Throws an error when renderToPath is not a string in any one of the config groups', () => {
-    const newConfig = {
-      ...sezzleConfig.new
-    };
+    const newConfig = cloneDeep(sezzleConfig.new);
     newConfig.configGroups[0].renderToPath = [newConfig.configGroups[0].renderToPath];
 
     expect(() => {
@@ -66,16 +57,14 @@ describe('Config validator function works as expected', () => {
   });
 
   test('Throws an error when a property which does not belong to a config group is being defined in a config group', () => {
-    const newConfig = {
-      ...sezzleConfig.new
-    };
-    newConfig.configGroups[0].merchantID = "someMerchantID";
+    const newConfig = cloneDeep(sezzleConfig.new);
+    newConfig.configGroups[0].merchantID = 'someMerchantID';
 
     expect(() => {
       Helper.validateConfig(newConfig);
-    }).toThrow(new Error("merchantID is not a property of a configGroup. Specify this key at the outermost layer"));
+    }).toThrow(new Error('merchantID is not a property of a configGroup. Specify this key at the outermost layer'));
   });
-})
+});
 
 describe('Constructor correctly sets the parameters', () => {
   test('Properly sets xpath value when there are multiple configs in the configGroups array', () => {
@@ -85,14 +74,14 @@ describe('Constructor correctly sets the parameters', () => {
         Helper.breakXPath(sezzleConfig.new.configGroups[i].targetXPath)
       );
     }
-  })
+  });
 
   test('Properly sets rendertopath value when there are multiple configs in the configGroups array', () => {
     const sz = new SezzleJS(sezzleConfig.new);
     for (var i = 0, len = sz.configGroups.length; i < len; i++) {
       expect(sz.configGroups[i].rendertopath).toEqual(sezzleConfig.new.configGroups[i].renderToPath);
     }
-  })
+  });
 
   test(`Properly sets ignoredPriceElements ` +
     `value when options.configGroups[i].ignoredPriceElements is` +
@@ -108,7 +97,7 @@ describe('Constructor correctly sets the parameters', () => {
         expect(sz.configGroups[i].ignoredPriceElements)
           .toEqual([['#id', '.class']]);
       }
-    })
+    });
 
   test(`Properly sets ignoredPriceElements ` +
     `value when options.configGroups[i].ignoredPriceElements is` +
@@ -118,7 +107,7 @@ describe('Constructor correctly sets the parameters', () => {
         expect(sz.configGroups[i].ignoredPriceElements)
           .toEqual([['#id-4', '.class-4'], ['#id-5', '.class-5']]);
       }
-    })
+    });
 
   test(`Properly duplicates ignoredPriceElements ` +
     `value when old config is passed and options.ignoredPriceElements is` +
@@ -132,7 +121,7 @@ describe('Constructor correctly sets the parameters', () => {
         expect(sz.configGroups[i].ignoredPriceElements)
           .toEqual([['#id', '.class']]);
       }
-    })
+    });
 
   test(`Properly duplicates ignoredPriceElements ` +
     `value when old config is passed and options.ignoredPriceElements is` +
@@ -142,5 +131,5 @@ describe('Constructor correctly sets the parameters', () => {
         expect(sz.configGroups[i].ignoredPriceElements)
           .toEqual([['#id-4', '.class-4'], ['#id-5', '.class-5']]);
       }
-    })
-})
+    });
+});
