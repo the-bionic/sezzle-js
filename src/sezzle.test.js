80d8c8274f5f5e64fbaebb3d81e318ee99de7d5f
jan-wei.lim <jan-wei.lim@sezzle.com>
2019-06-19 13:37:06 -0500

    Added tests to test correctness of Helper.validateConfig

--- src/sezzle.test.js
+++ src/sezzle.test.js
@@ -2,6 +2,74 @@ const SezzleJS = require('./sezzle');
 const Helper = require('./helper');
 var sezzleConfig = require('./sezzle.config.js');
 
+describe('Config validator function works as expected', () => {
+  test('Throws an error when configGroups is not an array', () => {
+    const newConfig = {
+      ...sezzleConfig.new
+    };
+    newConfig.configGroups = "somethingWhichIsNotAnArray";
+
+    expect(() => {
+      Helper.validateConfig(newConfig);
+    }).toThrow(new Error('options.configGroups must be an array'));
+  });
+
+  test('Throws an error when configGroups is an empty array', () => {
+    const newConfig = {
+      ...sezzleConfig.new
+    };
+    newConfig.configGroups = [];
+
+    expect(() => {
+      Helper.validateConfig(newConfig);
+    }).toThrow(new Error('options.configGroups must have at least one config object'));
+  });
+
+  test('Throws an error when targetXPath is not specified in any one of the config groups', () => {
+    const newConfig = {
+      ...sezzleConfig.new
+    };
+    delete newConfig.configGroups[0].targetXPath;
+
+    expect(() => {
+      Helper.validateConfig(newConfig);
+    }).toThrow(new Error('targetXPath must be specified in all configs in options.configGroups'));
+  });
+
+  test('Throws an error when renderToPath is not a string in any one of the config groups', () => {
+    const newConfig = {
+      ...sezzleConfig.new
+    };
+    newConfig.configGroups[0].targetXPath = [newConfig.configGroups[0].targetXPath];
+
+    expect(() => {
+      Helper.validateConfig(newConfig);
+    }).toThrow(new Error('renderToPath must be of type string'));
+  });
+
+  test('Throws an error when renderToPath is not a string in any one of the config groups', () => {
+    const newConfig = {
+      ...sezzleConfig.new
+    };
+    newConfig.configGroups[0].renderToPath = [newConfig.configGroups[0].renderToPath];
+
+    expect(() => {
+      Helper.validateConfig(newConfig);
+    }).toThrow(new Error('renderToPath must be of type string'));
+  });
+
+  test('Throws an error when a property which does not belong to a config group is being defined in a config group', () => {
+    const newConfig = {
+      ...sezzleConfig.new
+    };
+    newConfig.configGroups[0].merchantID = "someMerchantID";
+
+    expect(() => {
+      Helper.validateConfig(newConfig);
+    }).toThrow(new Error("merchantID is not a property of a configGroup. Specify this key at the outermost layer"));
+  });
+})
+
 describe('Constructor correctly sets the parameters', () => {
   test('Properly sets xpath value when there are multiple configs in the configGroups array', () => {
     const sz = new SezzleJS(sezzleConfig.newConfig);
