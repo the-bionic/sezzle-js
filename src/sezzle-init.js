332ffbda98b829ff6a06e264a82abd2e979956f4
Rachel Peddie <rachel.peddie@sezzle.com>
2019-07-17 11:03:13 -0500

    removed empty lines for correct formatting

--- src/sezzle-init.js
+++ src/sezzle-init.js
@@ -2,7 +2,5 @@ var SezzleJS = require('./sezzle');
 
 // Assumes document.sezzleConfig is present
 window.addEventListener("load", function() {
-
 	new SezzleJS(document.sezzleConfig).init();
-
 }, false );
